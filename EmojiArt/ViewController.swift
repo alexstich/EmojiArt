f29eb17b4376398789a300a351fc35117fade727
AlexStich <alex@rucode.org>
2020-09-11 14:26:16 +0300

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Алексей Гребенкин on 11.09.2020.
+//  Copyright © 2020 Alex Grebenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
